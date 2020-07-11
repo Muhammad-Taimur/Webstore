c0937eab9e228bbf60da47a87e4a6d9a18199bb3
Muhammad-Taimur <muhammadtaimurhassan@gmail.com>
2020-07-11 23:27:10 +0100

    Adding Order ID in Order Product Model.

--- WebStoreWeb/Models_View/OrderProduct.cs
+++ WebStoreWeb/Models_View/OrderProduct.cs
@@ -7,7 +7,7 @@ namespace WebStoreWeb.Models_View
 {
     public class OrderProduct
     {
-       // public int OrderId { get; set; }
+       //public int OrderId { get; set; }
         public int ProductId { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
